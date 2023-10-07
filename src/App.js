82a79301779affb2732b931cf65014b9b5e115f9
Midhun raj <midhunrajani686@gmail.com>
2023-10-07 18:46:52 +0530

    set-up

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I HAVE STARTED LEARNING
         </p>
         <a
           className="App-link"
